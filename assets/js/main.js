4ead65e4121275011ce9d8568a0a5d5d27b5c931
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-02-25 18:04:47 +0300

    search function is added

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,43 @@ function getValues(e){
     const checkoutValue = checkout.value
     searchFunction(destinationValue, adultValue, childrenValue, checkinValue, checkoutValue)
 }
-function searchFunction(destinationValue, adultValue, childrenValue, checkinValue, checkoutValue){
+async function searchFunction(destinationValue, adultValue, childrenValue, checkinValue, checkoutValue){
+    let hotelName = '';
+	let destinationId = '';
+	let hotelList = [];
 
+	const query = `locations/search?query=${destinationValue}`;
+	let result = await getData(query);
+	let hotels = result["suggestions"][1]['entities'];
+
+	
+	for (let i = 0; i < hotels.length; i++) {
+		const element = hotels[i];
+		destinationId = element['destinationId']
+
+		hotelName = element['name']
+		let hotel = await get_details(destinationId);
+		hotel.name = hotelName;
+		hotelList.push(hotel);
+		
+	}
+
+	
+	
+	console.log(hotelList);
+}
+
+async function getData(query){
+
+	let response =  await fetch(`https://hotels4.p.rapidapi.com/${query}`, {
+		"method": "GET",
+		"headers":  {
+			"x-rapidapi-key": "7fcd0ac249mshb557fe351c75ec4p1ffb75jsnc7d2a4272236",
+			"x-rapidapi-host": "hotels4.p.rapidapi.com"
+		}
+	})
+	
+	let data = await response.json()
+	return data
+	
 }
