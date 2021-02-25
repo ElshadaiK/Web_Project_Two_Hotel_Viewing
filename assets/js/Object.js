d1dd9774d2dcc1b9114baade3ca022fbdd51825e
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-02-25 18:07:13 +0300

    hotel object

---
+++ assets/js/Object.js
@@ -0,0 +1,36 @@
+class Hotel{
+    name = ""
+    hotelStar=""
+    neighbourhood=""
+    price=""
+    transportation={
+        distance:"",
+        distanceInTime:"",
+        name:""
+    }
+    guestReviews={
+        badgeText:"",
+        formattedRating:"",
+    }
+    freeService=""
+    address={
+        addressLine1: "",
+        addressLine2: "",
+        cityName: "",
+        countryCode: "",
+        countryName: "",
+        fullAddress: "",
+        pattern: "",
+        postalCode: "",
+        provinceName:""
+    }
+    room=[]
+
+    constructor(){
+        
+    }
+    
+}
+
+
+
