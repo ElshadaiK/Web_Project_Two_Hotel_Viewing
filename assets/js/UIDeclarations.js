548bfd44f3e2a0f5a7534a1ac111f7a902ce2054
Eden Berhan Alem <edenberhanalem@gmail.com>
2021-02-28 09:43:23 +0300

    Declared UI varaibles in another file considering separation of concerns

---
+++ assets/js/UIDeclarations.js
@@ -0,0 +1,12 @@
+const register = document.querySelector('.register');
+const confirm = document.querySelector('.confirm');
+const forgot = document.querySelector('.forgot');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const sign = document.querySelector("#sign");
+const reg = document.querySelector("#reg");
+const email_input = document.querySelector('.email_input');
+const password_input = document.querySelector('.password_input');
+const confirm_input = document.querySelector('.confirm_input');
+const header_text = document.querySelector('.header_text');
+
